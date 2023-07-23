b7cfd04031a2eaf340ed99368200106c5029f4b3
Ayush Srivastav <67184260+kraken426@users.noreply.github.com>
2023-07-24 00:41:06 +0530

    Update error_statement_handeling.sol

--- ETH + AVAX PROOF: Intermediate EVM Course/Module: Functions and Errors - ETH + AVAX/error_statement_handeling.sol
+++ ETH + AVAX PROOF: Intermediate EVM Course/Module: Functions and Errors - ETH + AVAX/error_statement_handeling.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SmartContract {
+    uint256 public balance;
+    constructor() {
+        balance = 600;
+    }
+    function deposit(uint256 amount) external {
+        require(amount > 0, "INVALID! Amount is less than zero");
+        balance += amount;
+    }
+    function withdraw(uint256 amount) external {
+        assert(amount <= balance);
+        balance -= amount;
+        if (amount > 100) {
+            revert("Withdrawal amount shoudnt be more than 100. You could be getting scammed!");
+        }
+    }
+}
 
